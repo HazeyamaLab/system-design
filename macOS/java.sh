2e1b6e04ebefd3574b7ce289b9a46bc261dace35
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-28 20:47:15 +0900

    feat: AdoptOpenJDK/openjdkでjava1.8とjava11をインストールするスクリプトの配置

---
+++ macOS/java.sh
@@ -0,0 +1,13 @@
+﻿# !/bin/bash
+
+brew tap homebrew/cask
+brew tap AdoptOpenJDK/openjdk
+
+echo "Java 1.8 をインストール中です..."
+brew cask adoptopenjdk/openjdk/adoptopenjdk8
+echo "Java 11 をインストールします..."
+brew cask adoptopenjdk11
+
+java -version
+
+echo "完了しました✨"
