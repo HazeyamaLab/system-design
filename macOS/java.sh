3861305bdcfa46e36735ca524e19c4d3251aba47
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-28 22:08:28 +0900

    feat: javaのバージョンをbashrcに書き込む

--- macOS/java.sh
+++ macOS/java.sh
@@ -8,6 +8,16 @@ brew cask install adoptopenjdk/openjdk/adoptopenjdk8
 echo "Java 11 をインストールします..."
 brew cask install adoptopenjdk11
 
+echo "JAVA_HOMEの設定を行います..."
+JAVA_HOME=$(/usr/libexec/java_home -v 11)
+
+cat <<EOF >> ~/.bashrc
+export JAVA_HOME=`${JAVA_HOME}`
+PATH=${JAVA_HOME}/bin:${PATH}
+EOF
+
+source ~/.bashrc
+
 java -version
 
 echo "完了しました✨"
