f84513e5c3c081dda0ab33a066e33bdd977b9b86
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-29 15:43:12 +0900

    feat: javaのインストールスクリプトにgradleのインストールを追加

--- macOS/java.sh
+++ macOS/java.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Gradleのインストール関数(6.3をインストール後に6.2.2にする)
+function install_gradle() {
+  brew insatll gradle
+  cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/ || return
+  git checkout 5fd374b706a7949f13fc20c654764c2ac5986e42 gradle.rb
+  brew unlink gradle
+  brew insatll gradle
+}
+
 brew tap homebrew/cask
 brew tap AdoptOpenJDK/openjdk
 
@@ -13,12 +22,15 @@ JAVA_HOME=$(/usr/libexec/java_home -v 11)
 
 cat <<EOF >> ~/.bashrc
 export JAVA_HOME=${JAVA_HOME}
-PATH=${JAVA_HOME}/bin:${PATH}
+PATH=$JAVA_HOME/bin:${PATH}
 EOF
 
 # shellcheck disable=SC1091
 source ~/.bashrc
 
-java -version
+# gradleのインストール
+install_gradle
+
+gradle -version
 
 echo "完了しました✨"
