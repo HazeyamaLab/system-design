22699e4865c8bf36f517df67d11d4381dfc7e61b
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-29 12:42:31 +0900

    fix: ciのファイルを分ける😢

--- windows/java.ps1
+++ windows/java.ps1
@@ -1,6 +1,15 @@
 Write-Output "現在のJAVA_HOMEを確認します..."
 Write-Output $ENV:JAVA_HOME
 
+# scoopのインストール
+if (!(Get-Command scoop -ea SilentlyContinue)) {
+  Write-Output "scoop をインストールしています..."
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+  scoop install git
+  scoop bucket add extras
+  scoop bucket add java
+}
+
 scoop install adopt8-hotspot
 scoop install adopt11-hotspot
 scoop reset adopt11-hotspot
