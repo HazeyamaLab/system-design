a874ca514867a80226ce8e87f570b188c737c1af
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-29 10:07:37 +0900

    fix: scoopが使えない問題にとりあえずif文で対処してみる

--- windows/java.ps1
+++ windows/java.ps1
@@ -1,6 +1,13 @@
 ﻿Write-Output "現在のJAVA_HOMEを確認します..."
 Write-Output $ENV:JAVA_HOME
 
+# scoopのインストール
+if (Get-Command scoop -ne SilentlyContinue) {
+  Write-Output "[1/8] scoop をインストールしています..."
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+  scoop bucket add java
+}
+
 scoop install adopt8-hotspot
 scoop install adopt11-hotspot
 scoop reset adopt11-hotspot
