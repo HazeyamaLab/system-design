a8bfc0b49af965b32f4315096b30d05c6879e3b2
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-29 15:51:45 +0900

    feat: lintのチェックを追加

--- windows/java.ps1
+++ windows/java.ps1
@@ -10,6 +10,7 @@ if (!(Get-Command scoop -ea SilentlyContinue)) {
   scoop bucket add java
 }
 
+# javaのインストール
 scoop install adopt8-hotspot
 scoop install adopt11-hotspot
 scoop reset adopt11-hotspot
@@ -18,6 +19,9 @@ Write-Output "変更後のJAVA_HOMEを確認します..."
 Write-Output $ENV:JAVA_HOME
 
 # Gradleのインストール
+Write-Output "Gradleをインストールしています..."
 scoop install gradle@6.2.2
 
 gradle -version
+
+Write-Output "完了しました✨"
