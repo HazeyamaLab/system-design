bb1074e7c31713c22823a3b7d18b1d37c1ad3343
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-29 10:33:09 +0900

    fix: 文字コードをutf-8に変更😢

--- windows/java.ps1
+++ windows/java.ps1
@@ -1,12 +1,5 @@
-﻿Write-Output "現在のJAVA_HOMEを確認します..."
+Write-Output "現在のJAVA_HOMEを確認します..."
 Write-Output $ENV:JAVA_HOME
-
-# scoopのインストール
-if (!(Get-Command scoop -ea SilentlyContinue)) {
-  Write-Output "[1/8] scoop をインストールしています..."
-  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-  scoop bucket add java
-}
 
 scoop install adopt8-hotspot
 scoop install adopt11-hotspot
