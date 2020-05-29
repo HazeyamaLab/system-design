722bd78ee61224934bfe13d83b54ae53ae1f7a16
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-29 09:32:36 +0900

    feat: windwos向けにjavaをインストールするスクリプトの用意

---
+++ windows/java.ps1
@@ -0,0 +1,9 @@
+﻿Write-Output "現在のJAVA_HOMEを確認します..."
+Write-Output $ENV:JAVA_HOME
+
+scoop install adopt8-hotspot
+scoop install adopt11-hotspot
+scoop reset adopt11-hotspot
+
+Write-Output "変更後のJAVA_HOMEを確認します..."
+Write-Output $ENV:JAVA_HOME
