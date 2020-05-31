84fd884118ce0760ede45dc5409c64d51c2c79ac
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-31 16:25:13 +0900

    feat: mysql 5.7 => 8系にするスクリプトの作成

---
+++ macOS/mysql.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+# MySQLのインストール
+if which mysql >/dev/null 2>&1; then
+  # serverの起動
+  mysql.server start
+
+  # データベースのバックアップ
+  mysqldump -u root -p -h 127.0.0.1 -A > backup.sql
+
+  # serverの停止
+  mysql.server stop
+fi
+
+brew reinstall mysql
+
+mysql --version
