b769e53dcc610fd6b5617d820050545a3f02e21e
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-31 16:48:57 +0900

    fix: 文字コードの変更

--- windows/mysql.ps1
+++ windows/mysql.ps1
@@ -1,4 +1,4 @@
-﻿if (Get-Command choco -ea SilentlyContinue) {
+if (Get-Command choco -ea SilentlyContinue) {
   choco install -y mysql
 } else {
   Set-ExecutionPolicy Bypass -Scope Process -Force;[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
