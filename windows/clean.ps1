8a895550e53990222e01ce574fba52979dbb2761
keychi25 <e165412y@st.u-gakugei.ac.jp>
2020-06-03 20:45:43 +0900

    fix : clean.ps1

--- windows/clean.ps1
+++ windows/clean.ps1
@@ -2,7 +2,7 @@
 if (Get-Command scoop -ea SilentlyContinue) {
   Write-Output "scoop をアンインストールします..."
   scoop uninstall scoop
-  Remove-Item -Force -Recurse -Path "$HOEM\scoop\**"
+  Remove-Item -Force -Recurse -Path "$HOME\scoop\**"
 } else {
   Write-Output "scoopがインストールされていません．スクリプトを終了します．"
 }
